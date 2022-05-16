9e46382ec5290dfe95ff25682b89cfd0b0ec1560
Ibra95ak-pomechain <ibrahim.aboukhalil95@gmail.com>
2022-05-16 14:41:37 +0400

    Design updates and PHPMailer installation

--- mail.php
+++ mail.php
@@ -1,6 +1,13 @@
 <?php
 
     header("Access-Control-Allow-Origin: *");
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\SMTP;
+    use PHPMailer\PHPMailer\Exception;
+
+    //Load Composer's autoloader
+    require 'vendor/autoload.php';
+
     $form = $_GET["form"];
     switch ($form){
         case "home":
@@ -41,20 +48,31 @@
             break;
     }
 
-        // Set the recipient email address.
-        $recipient = "ibrahimakdxb@gmail.com";
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
 
-        // Set the email subject.
-        $subject = "New contact from Form [".$form."]";
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                       //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'ibrahim.aboukhalil@pomechain.com';     //SMTP username
+    $mail->Password   = 'dxb@pomechain2022';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
+    //Recipients
+    $mail->setFrom('ibrahim.aboukhalil@pomechain.com', 'Mailer');
+    $mail->addAddress($email, $name);     //Add a recipient
 
-        // Build the email headers.
-        $email_headers = "From: $name <$email>";
+    //Content
+    $mail->Subject = 'New contact from Form ['.$form.']';
+    $mail->Body    = $email_content;
 
-        // Send the email.
-        if (mail($recipient, $subject, $email_content, $email_headers)) {
-            return true;
-        } else {
-            return false;
-        }
+    $mail->send();
+    return true;
+} catch (Exception $e) {
+    return false;
+}
 ?>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'aa1d9b1488bd7e1bcfe23ff3e5c39eaa97f95b33',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'aa1d9b1488bd7e1bcfe23ff3e5c39eaa97f95b33',
+    ),
+    'phpmailer/phpmailer' => 
+    array (
+      'pretty_version' => 'v6.6.0',
+      'version' => '6.6.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e43bac82edc26ca04b36143a48bde1c051cfd5b1',
+    ),
+  ),
+);
